2.2.4.20220920.21523778: Fixed area computation

illum/PolarArray.py
@@ -59,8 +59,11 @@ class PolarArray:
             self._cRad,
             np.hstack([0, (np.arange(self._rmax) + 0.5)]) / self._rmax,
         )
-        dr = np.diff(r)[self._rmin :]
-        return 2 * np.pi / self.data.shape[0] * dr * self._scale * self.radii()
+        return (
+            (np.pi / self.data.shape[0])
+            * np.diff(r**2)[self._rmin :]
+            * self._scale**2
+        )
 
     def clip(self, radius):
         parr = copy(self)

illum/UI/warp.py
@@ -2,17 +2,12 @@
 
 import os
 from glob import glob
-from subprocess import call
 
 import numpy as np
-import yaml
-from PIL import Image
+import rasterio as rio
 
+import illum.PolarArray as PA
 from illum import MultiScaleData as MSD
-
-
-def OpenTIFF(path):
-    return np.array(Image.open(path))
 
 
 def warp_files(srcfiles, projection, extent):