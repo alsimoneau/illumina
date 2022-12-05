d2bd78a1deb73fd5d979c3efe81e79ac3b8f0eeb
Alexandre Simoneau <alsimoneau@gmail.com>
2022-12-05 13:00:26 -0500

    Adjusted constants precision

--- illum/compute/transmitance.f90
+++ illum/compute/transmitance.f90
@@ -9,13 +9,13 @@ FUNCTION trans(zenith_angle, z_i, z_f, distd, scale_height, trana)
 
   REAL(8) :: trans, z1, z2
 
-  IF (distd == 0.0) THEN
-    trans = 1.0
+  IF (distd == 0) THEN
+    trans = 1
     RETURN
   END IF
 
   IF (z_i == z_f) THEN
-    trans = EXP((LOG(trana)) * EXP(-1.0 * z_i / scale_height) * distd / scale_height)
+    trans = EXP((LOG(trana)) * EXP(-1 * z_i / scale_height) * distd / scale_height)
     RETURN
   END IF
 
@@ -28,13 +28,13 @@ FUNCTION trans(zenith_angle, z_i, z_f, distd, scale_height, trana)
   END IF
 
   trans = EXP((LOG(trana) / ABS(COS(zenith_angle))) &
-              * (EXP(-1.0 * z1 / scale_height) - EXP(-1.0 * z2 / scale_height)))
+              * (EXP(-1 * z1 / scale_height) - EXP(-1 * z2 / scale_height)))
 
-  IF (trans == 0.0) THEN
+  IF (trans == 0) THEN
     PRINT *, 'ERREUR trans - no transmission', z_i, z_f, zenith_angle, trana, distd, scale_height
   END IF
 
-  IF (trans > 1.0) THEN
+  IF (trans > 1) THEN
     PRINT *, 'ERREUR avec trans', trans, z_i, z_f, zenith_angle
     STOP
   END IF
@@ -49,10 +49,10 @@ FUNCTION trans_toa(pression, wavelength) RESULT(trans)
   REAL(8) :: trans
 
   !  From Bodhaine et al. (1999)
-  trans = EXP(-1.0 * pression / 101.325 * 0.0021520 &
-              * (1.0455996 - 341.29061 * (wavelength / 1000.0)**-2.0 &
-                 - 0.90230850 * (wavelength / 1000.0)**2.0) &
-              / (1.0 + 0.0027059889 * (wavelength / 1000.0)**-2.0 &
-                 - 85.968563 * (wavelength / 1000.0)**2.0))
+  trans = EXP(-1 * pression / 101.325D0 * 0.0021520D0 &
+              * (1.0455996D0 - 341.29061D0 * (wavelength / 1000.0D0)**-2 &
+                 - 0.90230850D0 * (wavelength / 1000.0D0)**2) &
+              / (1 + 0.0027059889D0 * (wavelength / 1000.0D0)**-2 &
+                 - 85.968563D0 * (wavelength / 1000.0D0)**2))
 
 END FUNCTION
